9025d22ebcdf927c31139962fd42a75bf250b42e
Afief Sholichuddin <apiep.oedin@gmail.com>
2017-09-26 14:29:42 +0700

    Add some comments and some minor improvement

--- js/main.js
+++ js/main.js
@@ -57,42 +57,68 @@ function loadRoomList () {
 function removeOverlay () {
   $('.overlay').css({display: 'none'})
 }
+// Here we attach event listener on the chat bubble
+// to prevent the default behavior of it, instead of
+// opening new tab based on the link
+// it will open a new "mini" window
 function attachBubbleClickListener () {
-  console.log('attach event')
+  // Regexp to filter if the chat bubble is really something
+  // that we want to prevent
+  // url comming from android "deep-link":
+  // qiscus://com.android.streamer/ROOM_NAME
   var reRoomName = /^qiscus:\/\/com\.android\.streamer\/([a-zA-Z0-9]+)/i
-  var reRTMP = /rtmp:\/\/(\S+)/
+  // "mini" window video viewer url
   var viewerURL = window.location.href + '/viewer.html?url=rtmp://rtc.qiscus.com:2935/live360p/'
 
+  // Here we attach the click event listener to link
+  // that href attribute start with `qiscus`
   $('body').on('click', 'a[href^=qiscus]', function (event) {
-    console.group('action-buttons')
     event.preventDefault()
     event.stopPropagation()
-    console.log('click')
+    // here we get the real href from chat bubble
     var text = $(this).attr('href')
-    console.log('text', text)
+    // ... then get the real url
     var isRTMP = text.match(reRoomName)
+    // ... if it is the url we want continue, else skip
     if (isRTMP) {
-      var rtmpURL = isRTMP[1]
-      var fullURL = viewerURL + rtmpURL
-      console.log('viewer-url', fullURL)
+      // ... get the room name from the url, because that what we only needed
+      var roomName = isRTMP[1]
+      // ... if it was a request stream bubble prevent the user
+      // and show a modal, about why it is being prevented
+      if (roomName === 'request') {
+        $('.modal-container').toggleClass('hidden')
+        return
+      }
+      // ... build the full url, and open the "mini" window,
+      // to show the video previewer
+      var fullURL = viewerURL + roomName
       window.open(fullURL, 'Viewer', 'modal=1,status=0,height=600,width=800,location=0')
-      console.groupEnd('action-buttons')
       return false
     }
-    console.groupEnd('action-buttons')
   })
 }
+// This function is used to send a request stream to the other participants
 function handleRequestButtonClick (event) {
   event.preventDefault()
+  // ... here we generate a uniqueId for the message
+  // hence I just using timestamp, because it is always different, and
+  // easy to get.
   var uniqueId = new Date().getTime()
-  var roomName = 'testing1'
+  // ... room name for the requested stream
+  // here we use "request" as room name as requested by android developer
+  var roomName = 'request'
+  // here is the "payload" for posting postback comment / message
   var payload = {
+    // text to appear inside the bubble
     text: 'Hi, I requested a video streaming',
+    // button configuration as array (yep, you can have more than 1 button)
     buttons: [
       {
-        label: 'Open now',
+        // button label
+        label: 'Start stream',
         type: 'link',
         payload: {
+          // android "deep-link" scheme
           url: 'qiscus://com.android.streamer/' + roomName
         }
       }
@@ -100,10 +126,15 @@ function handleRequestButtonClick (event) {
   }
   var stringifiedPayload = JSON.stringify(payload)
   qiscus.submitComment(
+    // Current roomId
     qiscus.selected.id,
+    // Comment text
     'Hi aku request video streaming',
+    // uniqueId
     uniqueId,
+    // comment type (default to "text")
     'buttons',
+    // payload (optional, only used when posting non-text comment)
     stringifiedPayload
   )
 }
@@ -112,4 +143,8 @@ $(document).ready(function () {
   init()
   attachBubbleClickListener()
   $('body').on('click', '.request-btn', handleRequestButtonClick)
+  $('body').on('click', '.modal-container .close-btn', function (event) {
+    event.preventDefault()
+    $('.modal-container').addClass('hidden')
+  })
 })
